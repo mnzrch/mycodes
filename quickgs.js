0ab500f5f8d1c339e16c9d5950434950b92d43b4
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2023-02-25 09:22:01 +0500

    Add files via upload

---
+++ quickgs.js
@@ -0,0 +1,19 @@
+let qget_started = `<div class="qget_started lightboxT w-fitcon h-fitcon">
+  <header class="lightele w-wfa">Quick Get Started...</header>
+  <div class="con lightboxLG w-fitcon ta-center">
+    If You are First Time Visiting this site Then this Quick Guide will help you to get started.
+    <br /><img alt="smart menu indicator" src="./assests/sm.png"><br />
+    All Covered Courses Will be founded in smart menu which is marked in above Picture.
+  </div>
+  <footer class="lightele w-wfa dis-flex jc-spacebetween ai-center">
+    <con class="dis-flex ai-center"><span class="checkbox" value=""></span>Don't show this message again</con>
+    <ctrl><span class="btnv3">Close</span></ctrl>
+  </footer>
+</div>`;
+          if (getCoki('qget_started') != 1) {
+               document.write(qget_started);
+          }
+          click(function () {
+               if (getAttr('value', $(' div.qget_started.lightboxT.w-fitcon.h-fitcon > footer > con > span')) == 1) { setCoki('qget_started', '1', { 'max-age': '31104000' }) }
+               $('div.qget_started').remove();
+          }, $('div.qget_started.lightboxT.w-fitcon.h-fitcon > footer > ctrl > span'));
