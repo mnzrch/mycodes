75b364ba8adb04a1ff17857ae1065ec9d93bbd77
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2023-02-25 10:49:58 +0500

    Update quickgs.js

--- quickgs.js
+++ quickgs.js
@@ -7,16 +7,14 @@ let qget_started = `<style>.qget_started {position: fixed;top: 50%;left: 50%;tra
     All Covered Courses Will be founded in smart menu which is marked in above Picture.
   </div>
   <footer class="lightele w-wfa dis-flex jc-spacebetween ai-center">
-    <con class="dis-flex ai-center"><span title="Please wait, Item is being loaded!" class="checkbox cursor-notallowed" value=""></span>Don't show this message again</con>
+    <con class="dis-flex ai-center"><span title="Please wait, Item is being loaded!" class="checkbox" value=""></span>Don't show this message again</con>
     <ctrl><span class="btnv3">Close</span></ctrl>
   </footer>
 </div>`;
 if (getCoki('qget_started') != 1) {
   document.write(qget_started);
-  let closbox = tag('div.qget_started.lightboxT.w-fitcon.h-fitcon > footer > con > span');
-  window.onload = function(){setCSS('cursor', 'pointer', closbox); setAttr('title', 'Click to check/uncheck', closbox);clog(closbox)};
   click(function () {
-    if (getAttr('value', closbox) == 1) { setCoki('qget_started', '1', { 'max-age': '31104000' }) }
+    if (getAttr('value', tag('div.qget_started.lightboxT.w-fitcon.h-fitcon > footer > con > span')) == 1) { setCoki('qget_started', '1', { 'max-age': '31104000' }) }
     tag('div.qget_started').remove();
   }, tag('div.qget_started.lightboxT.w-fitcon.h-fitcon > footer > ctrl > span'));
 }
