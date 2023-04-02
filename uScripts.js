e46b1cac49f4fddd8f7cb143062f656680551447
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2023-04-02 08:13:01 +0500

    Add files via upload

--- uScripts.js
+++ uScripts.js
@@ -12,14 +12,6 @@ if (smca) {
 function quickguid() {
     loading_12345.style.display = 'flex';
     loading_12345.style.opacity = '1';
-    function closeload() {
-        setTimeout(() => {
-            loading_12345.style.opacity = '0';
-            setTimeout(() => {
-                loading_12345.style.display = 'none';
-            }, 1000);
-        }, 1000);
-    }
     let lcources = [{
         "title": "HTML Full Cource with Exercise",
         "thumb": "https://i.ibb.co/cDG9MhV/HTML-Course-covering-by-Lets-Learn-Lights.jpg",
@@ -32,13 +24,11 @@ function quickguid() {
         "index": 2
     }];
     let guider = newEle(
-        `
-        <style>
+        `<style>
              .lll-corses {
                   height: 200px;
                   display: flex;
              }
-
              .lll-corses>a {
                 font-size: 3em;
                 line-height: 1.4em;
@@ -79,4 +69,22 @@ function quickguid() {
     })
     scripts();
     closeload();
-}
+};
+function llldls() {
+    let llldlsw = newEle(`
+    <div class="btnv3 btn-0url" url="">Download SSrMovies ads bypass by Letslearnlight</div>
+    `
+        , 'div'); setAttr('name', 'FreeWare Downloads from Letslearnlights', llldlsw);
+    setAttr('class', 'vp-center lightele dragable windowtype bs-hidden', llldlsw); $setCSS({ 'transition': '0s', 'z-index': '10000' }, llldlsw);
+    document.body.appendChild(llldlsw);
+    scripts();
+    closeload();
+};
+function closeload() {
+    setTimeout(() => {
+        loading_12345.style.opacity = '0';
+        setTimeout(() => {
+            loading_12345.style.display = 'none';
+        }, 1000);
+    }, 1000);
+};
