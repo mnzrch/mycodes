f0e18f6bd27d00a256c392e60f940b8ab5c9ec9e
Mnzr chauhan <95564253+mnzrch@users.noreply.github.com>
2023-04-02 08:45:02 +0500

    Add files via upload

--- uScripts.js
+++ uScripts.js
@@ -8,6 +8,9 @@ if (smca) {
 `, 'span');
     smca.appendChild(newspn);
     click(quickguid, newspn);
+    let dlwndw = newEle(`Download SSrMovies ads bypass by Letslearnlight`, 'div');setAttr('class','lightboxWS w-fitcon cursor-pointer margin-5 dis-inlineB',dlwndw)
+    smca.appendChild(dlwndw);
+    click(llldls, dlwndw);
 }
 function quickguid() {
     loading_12345.style.display = 'flex';
@@ -71,8 +74,10 @@ function quickguid() {
     closeload();
 };
 function llldls() {
+    loading_12345.style.display = 'flex';
+    loading_12345.style.opacity = '1';
     let llldlsw = newEle(`
-    <div class="btnv3 btn-0url" url="">Download SSrMovies ads bypass by Letslearnlight</div>
+    <div class="btnv3 btn-0url" url="https://drive.google.com/u/0/uc?id=1rWC7EykyY7vFHOHbIJTbSqIYEpSqTpag&export=download">Download SSrMovies ads bypass by Letslearnlight</div>
     `
         , 'div'); setAttr('name', 'FreeWare Downloads from Letslearnlights', llldlsw);
     setAttr('class', 'vp-center lightele dragable windowtype bs-hidden', llldlsw); $setCSS({ 'transition': '0s', 'z-index': '10000' }, llldlsw);
